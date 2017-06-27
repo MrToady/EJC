2c97da11dd64f8904d9528e1ef1844dae9ce1cb2
Taras Moisiuk <t.v.moisiuk@gmail.com>
2017-06-27 15:34:14 +0300

    Text & Modifier added

---
+++ src/task_02/Modifier.java
@@ -0,0 +1,20 @@
+package task_02;
+
+public class Modifier {
+    public static int NUM_OF_PATRICIPANTS = 5;
+    public static double POSS_OF_SHOT_DUCK = 0.23;
+    public static int DISTANCE = 1000;
+
+
+    public static int MIN_SPEED = 10;
+    public static int MAX_SPEED = 100;
+
+
+
+    public static int START_CASH = 500;
+    public static int WIN_CASH = 200;
+    public static int STAKE_CASH = 100;
+
+
+
+}
---
+++ src/task_02/Text.java
@@ -0,0 +1,16 @@
+package task_02;
+
+public class Text {
+    public static final String HEALTHY_DUCK_DISPLAY = "Я здоровая утка!";
+    public static final String SHOT_DUCK_DISPLAY = "Я подстрелянная утка(((";
+    public static final String RASER_NUMBER = "Участник №%d: ";
+    public static final String RACE_WELCOME = "Начинается новый заезд! В этом заезде стартуют cледующие участники:";
+    public static final String RACERS_IS_READY = "Участники готовы к соревнованиям!";
+    public static final String READY = "На старт!";
+    public static final String STEADY = "Внимание!";
+    public static final String GO = "МАРШ!";
+    public static final String SITUATION = "Участник №%d на данный момент пролетел %d м. дистанции.";
+    public static final String IS_WINNER = "И у нас есть победитель! Первой прилетела утка №%d! Поздравляем победителя.";
+    public static final String EVERYBODY_IS_SHOT = "Извините, похоже у нас как-то не сложилось с участниками в этот раз.";
+
+}
