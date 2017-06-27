e41268fbabc8515d0132427054ae9cb4fa0c8b0f
Taras Moisiuk <t.v.moisiuk@gmail.com>
2017-06-27 17:40:42 +0300

    Text file updated

--- src/task_02/Text.java
+++ src/task_02/Text.java
@@ -1,16 +1,37 @@
 package task_02;
 
-public class Text {
-    public static final String HEALTHY_DUCK_DISPLAY = "Я здоровая утка!";
-    public static final String SHOT_DUCK_DISPLAY = "Я подстрелянная утка(((";
-    public static final String RASER_NUMBER = "Участник №%d: ";
-    public static final String RACE_WELCOME = "Начинается новый заезд! В этом заезде стартуют cледующие участники:";
-    public static final String RACERS_IS_READY = "Участники готовы к соревнованиям!";
-    public static final String READY = "На старт!";
-    public static final String STEADY = "Внимание!";
-    public static final String GO = "МАРШ!";
-    public static final String SITUATION = "Участник №%d на данный момент пролетел %d м. дистанции.";
-    public static final String IS_WINNER = "И у нас есть победитель! Первой прилетела утка №%d! Поздравляем победителя.";
-    public static final String EVERYBODY_IS_SHOT = "Извините, похоже у нас как-то не сложилось с участниками в этот раз.";
+class Text {
+    static final String HEALTHY_DUCK_DISPLAY = "Я здоровая утка!";
+    static final String SHOT_DUCK_DISPLAY = "Я подстрелянная утка(((";
+    static final String RAСER_NUMBER = "Участник №%d: ";
+    static final String RACE_WELCOME = "Начинается новый заезд! В этом заезде стартуют cледующие участники:";
+    static final String RACERS_IS_READY = "Участники готовы к соревнованиям!";
+    static final String READY = "На старт!";
+    static final String STEADY = "Внимание!";
+    static final String GO = "МАРШ!";
+    static final String SITUATION = "Участник №%d на данный момент пролетел %d м. дистанции.";
+    static final String IS_WINNER = "И у нас есть победитель! Первой прилетела утка №%d! Поздравляем победителя.";
+    static final String CASINO_WELCOME = "Здравствуйте, мы приветствуем вас в нашем казино! " + "\n" +
+     "Здесь вы можете сделать свои ставки на утиных гонках." + "\n" +
+     "Мы достаточно толерантно относимся к составу участников гонок, поэтому в них принимают участие" + "\n" +
+     "и подстрелянные утки. Проверьте, улыбнется ли вам фортуна!";
+    static final String CASINO_WANT_TO_BET = "Количество участников в наших гонках - %d." + "\n"
+     + "На участника с каким номером вы хотите поставить?";
+    static final String CASINO_NUMBER_FORMAT_EXCEPTION = "Корректно введите число участника на которого хотите поставить";
+    static final String CASINO_WRONG_NUMBER = "В этом заезде принимают участие %d участников. Вы должны поставить на одного из них.";
+    static final String CASINO_YOUR_BET = "Отлично! вы поставили на участника №%d. Давайте посмотрим, повезет ли ему.";
+    static final String CASINO_PLAY_AGAIN = "Хотели бы вы сыграть?";
+    static final String CASINO_START = "Отлично!";
+    static final String CASINO_NO_START = "Жаль. Рады были вас видеть.";
+    static final String CASINO_WRONG_ANSWER = "Введите 'д' для положительного ответа или 'н' - для отрицательного";
+    static final String CASINO_PLAYER_WIN = "Поздравляем, вы выиграли! Сумма на вашем счету увеличилась на 200$";
+    static final String CASINO_PLAYER_LOSE = "К сожалению, вы проиграли.";
+    static final String CASINO_PLAYER_CANNOT_PLAY = "К сожалению у вас недостаточно средств чтобы сделать ставку. Были рады вам!";
+    static final String PLAYER_BALANCE = "У вас на счету %d$.";
+
+
+
+
+
 
 }
