d13b030b5f20a87cd001fbc2e07621e64c9da66f
Taras Moisiuk <t.v.moisiuk@gmail.com>
2017-06-27 17:47:57 +0300

    Modifier updated

--- src/task_02/Modifier.java
+++ src/task_02/Modifier.java
@@ -1,20 +1,12 @@
 package task_02;
 
-public class Modifier {
-    public static int NUM_OF_PATRICIPANTS = 5;
-    public static double POSS_OF_SHOT_DUCK = 0.23;
-    public static int DISTANCE = 1000;
-
-
-    public static int MIN_SPEED = 10;
-    public static int MAX_SPEED = 100;
-
-
-
-    public static int START_CASH = 500;
-    public static int WIN_CASH = 200;
-    public static int STAKE_CASH = 100;
-
-
-
+class Modifier {
+    static int NUM_OF_PATRICIPANTS = 5;
+    static double POSS_OF_SHOT_DUCK = 0.23;
+    static int DISTANCE = 1000;
+    static int MIN_SPEED = 10;
+    static int MAX_SPEED = 100;
+    static int START_CASH = 500;
+    static int WIN_CASH = 200;
+    static int BET_CASH = 100;
 }
