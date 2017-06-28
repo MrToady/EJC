fb85647d5201224a37a113e6683cfa1fc9303c4b
Taras Moisiuk <t.v.moisiuk@gmail.com>
2017-06-28 13:35:50 +0300

    Text and Modifier updated, Javadoc added

--- src/task_02/Modifier.java
+++ src/task_02/Modifier.java
@@ -1,12 +1,15 @@
 package task_02;
 
-class Modifier {
-    static int NUM_OF_PATRICIPANTS = 5;
-    static double POSS_OF_SHOT_DUCK = 0.23;
-    static int DISTANCE = 1000;
-    static int MIN_SPEED = 10;
-    static int MAX_SPEED = 100;
-    static int START_CASH = 500;
-    static int WIN_CASH = 200;
-    static int BET_CASH = 100;
+/**
+ * Modifier class. Contains game settings.
+ */
+public class Modifier {
+    public static int NUM_OF_PATRICIPANTS = 5; /*Number of race patricipants*/
+    public static double POSS_OF_SHOT_DUCK = 0.23; /*Possibility of Shot Duck appearance*/
+    public static int DISTANCE = 1000; /*Race distance*/
+    public static int MIN_SPEED = 10; /*Minimal speed before circular shift*/
+    public static int MAX_SPEED = 100; /*Maximal speed before circular shift*/
+    public static int START_CASH = 500; /*Start balance*/
+    public static int WIN_CASH = 200; /*Winning amount*/
+    public static int BET_CASH = 100; /*Bet's value*/
 }
