2a15ea6a5bf24ac9537cbfddc367681e6fa13fe5
Taras Moisiuk <t.v.moisiuk@gmail.com>
2017-08-10 16:03:34 +0300

    "while" loop has been changed to awaitTermination()

--- src/main/java/task_10/TimeMapFiller.java
+++ src/main/java/task_10/TimeMapFiller.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class TimeMapFiller {
     private Map<String, Long> timeMap;
@@ -27,7 +28,11 @@ public class TimeMapFiller {
             executorService.execute(new ReadingThread(fileForParsing, timeMap));
         }
         executorService.shutdown();
-        while (!executorService.isTerminated());
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return timeMap;
     }
 }
